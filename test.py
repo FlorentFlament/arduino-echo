45618f6fd965311dd21d701b5aa8825679f91293
Florent Flament <contact@florentflament.com>
2022-10-26 16:55:39 +0200

    Make code work with Python3

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ import time
 ser = serial.Serial('/dev/ttyUSB0', 1000000, stopbits=1, timeout=1)
 time.sleep(2) # Wait for end of reset
 
-print ser.readline()
+print( ser.readline() )
 while True:
     sys.stdout.write('> ')
-    ser.write(sys.stdin.readline())
-    print ser.readline()
+    sys.stdout.flush()
+    ser.write( str.encode(sys.stdin.readline()) )
+    print( ser.readline() )
